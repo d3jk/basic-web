1199e88ed3f3ada26bdc7ee2c494fbc87fc9c77c
d3jk <d3kn@proton.me>
2025-07-26 18:01:26 -0500

    Working basic web app for LYDMD

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,28 +1,48 @@
-import Header from "./Header"
+import Header from './Header'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import 'animate.css';
 
 function About() {
+  const navigate = useNavigate()
+  const [animationSlider, setAnimationSlider] = useState('animate__animated animate__slideInLeft')
+  const handleClick = () => {
+    setAnimationSlider('animate__animated animate__slideOutLeft')
+    setTimeout(() => {
+      navigate('/')
+    }, 500)
+  }
   return (
     <>
-      <Header />
-      <h2>About LYDMD</h2>
-      <p>
-        Love You Des Moines Days is an annual 9-day celebration for our community 
-        with over 400 free, fun, and meaningful activities for all ages, 
-        hosted by the Valley Community Center. The Valley Community Center is owned and 
-        operated by Valley Church as a gift to the community and is a hub for partnerships 
-        doing good for the community…with the community. Learn more about the 
-        Valley Community Center.
-      </p>
-      <p>
-        During Love You Des Moines Days, Valley Church attenders, as well as community 
-        and partner organizations, provide a wide variety of workshops, activities, 
-        clinics, and social gatherings for our neighbors. From pickleball, to family 
-        splash bash, to cars & coffee, free bike repair, ice cream making, writing 
-        workshops, arts & crafts - there is something for everyone!
-      </p>
-      <p>
-        Join us July 26, through August 3, and beyond!
-      </p>
+      <header className={animationSlider}>
+        <Header />
+      </header>
+      <div className="body-container">
+        <h2>About LYDMD</h2>
+        <div className='about-container'>
+          <p className='about'>
+            Love You Des Moines Days is an annual 9-day celebration for our community 
+            with over 400 free, fun, and meaningful activities for all ages, 
+            hosted by the Valley Community Center. The Valley Community Center is owned and 
+            operated by Valley Church as a gift to the community and is a hub for partnerships 
+            doing good for the community…with the community. Learn more about the 
+            Valley Community Center.
+          </p>
+          <p className='about'>
+            During Love You Des Moines Days, Valley Church attenders, as well as community 
+            and partner organizations, provide a wide variety of workshops, activities, 
+            clinics, and social gatherings for our neighbors. From pickleball, to family 
+            splash bash, to cars & coffee, free bike repair, ice cream making, writing 
+            workshops, arts & crafts - there is something for everyone!
+          </p>
+          <p className='about'>
+            Join us July 26, through August 3, and beyond!
+          </p>
+        </div>
+        <button onClick={handleClick}>
+            Home
+        </button>
+      </div>
     </>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,66 @@
-import Header from "./Header"
-import { useNavigate } from "react-router-dom"
+import Header from './Header'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import 'animate.css';
 
 function Home() {
   const navigate = useNavigate()
+  const [animationSlider, setAnimationSlider] = useState('animate__animated animate__slideInRight')
+  const [bibleVerse, setBibleVerse] = useState('Click to generate a Bible verse')
+  const [verseRef, setVerseRef] = useState('')
+  const handleClick = () => {
+    setAnimationSlider('animate__animated animate__slideOutRight')
+    setTimeout(() => {
+      navigate('/about')
+    }, 500)
+  }
+
+  const generateBibleVerse = () => {
+    fetchData()
+    console.log('Bible verse generated:', bibleVerse)
+  }
+
+  const fetchData = async () => {
+    console.log('Fetching Bible verse...')
+      try {
+        const response = await fetch('https://bible-api.com/data/web/random/JHN');
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const verseObj = await response.json();
+        setBibleVerse(verseObj.random_verse.text);
+        setVerseRef('- ' + verseObj.random_verse.book + ' ' + verseObj.random_verse.chapter + ':' + verseObj.random_verse.verse);
+        console.log(verseObj); // Log the entire object for debugging
+      } catch (err) {
+        console.log(err);
+      } 
+    }
+      
   return (
     <>
-      <Header />
-      <h1>My Basic Web App</h1>
-      <div className="card">
-        <button onClick={() => navigate('/about')}>
-          Learn More
-        </button>
-        <p>
-          Click to learn more about LYDMD
-        </p>
+      <header className={animationSlider}>
+        <Header />
+      </header>
+      <div className="body-container">
+        <h1>Basic Web App</h1>
+        <div className='card'>
+          <button onClick={handleClick}>
+            Learn More
+          </button>
+          <p>
+            Click to learn more about LYDMD
+          </p>
+          <hr />
+          <button onClick={generateBibleVerse} style={{ marginTop: '1em' }}>
+            Generate Bible Verse
+          </button>
+          <p>
+            {bibleVerse}
+          </p>
+          <p>
+            <i>{verseRef}</i>
+          </p>
+        </div>
       </div>
     </>
   )
